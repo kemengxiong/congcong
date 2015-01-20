986b3a392ea9d7630ced3fe2012804d4411bc72a
kemengxiong <kemengxiong@gmail.com>
2015-01-20 17:43:47 -0500

    congcong
    initial source code

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/congcong/admin/');
+define('HTTP_CATALOG', 'http://localhost/congcong/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/congcong/admin/');
+define('HTTPS_CATALOG', 'http://localhost/congcong/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\xampp\htdocs\congcong/admin/');
+define('DIR_SYSTEM', 'C:\xampp\htdocs\congcong/system/');
+define('DIR_LANGUAGE', 'C:\xampp\htdocs\congcong/admin/language/');
+define('DIR_TEMPLATE', 'C:\xampp\htdocs\congcong/admin/view/template/');
+define('DIR_CONFIG', 'C:\xampp\htdocs\congcong/system/config/');
+define('DIR_IMAGE', 'C:\xampp\htdocs\congcong/image/');
+define('DIR_CACHE', 'C:\xampp\htdocs\congcong/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp\htdocs\congcong/system/download/');
+define('DIR_UPLOAD', 'C:\xampp\htdocs\congcong/system/upload/');
+define('DIR_LOGS', 'C:\xampp\htdocs\congcong/system/logs/');
+define('DIR_MODIFICATION', 'C:\xampp\congcong/system/modification/');
+define('DIR_CATALOG', 'C:\xampp\htdocs\congcong/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'congcong');
+define('DB_PREFIX', 'cc_');
---
+++ admin/view/template/common/footer.tpl
@@ -0,0 +1,5 @@
+<footer id="footer">
+<!---<?php echo $text_footer; ?>--->
+<?php echo "技术支持 丛丛中国网站"; ?>
+<br /><?php echo $text_version; ?></footer></div>
+</body></html>
